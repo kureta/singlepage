98c173264e43ae9e23dd3f7ae27406b9a2660a2d
kureta <skureta@gmail.com>
2024-02-13 14:32:08 +0300

    refactoring

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 import base64
 
+from selenium.common import JavascriptException
+
 # Set up headless Chrome browser
 options = webdriver.ChromeOptions()
 options.add_argument("--headless")
@@ -21,57 +23,53 @@ soup = BeautifulSoup(html_content, "html.parser")
 # Base URL of the webpage
 base_url = driver.current_url
 
+
+def fetch_image(resource_url):
+    if not resource_url.startswith("http"):
+        resource_url = driver.execute_script(
+            f"return new URL('{resource_url}', window.location.href).href"
+        )
+    try:
+        img_data = driver.execute_script(
+            f"return fetch('{resource_url}').then(response => response.blob()).then(blob => new Promise((resolve, reject) => {{"
+            f"    const reader = new FileReader();"
+            f"    reader.onloadend = () => resolve(reader.result);"
+            f"    reader.onerror = reject;"
+            f"    reader.readAsDataURL(blob);"
+            f"}}));"
+        )
+    except JavascriptException:
+        img_data = ""
+    return img_data
+
+
+def fetch_text(resource_url):
+    if not resource_url.startswith("http"):
+        resource_url = driver.execute_script(
+            f"return new URL('{resource_url}', window.location.href).href"
+        )
+    try:
+        content = driver.execute_script(
+            f"return fetch('{resource_url}').then(response => response.text());"
+        )
+    except JavascriptException:
+        content = ""
+    return content
+
+
 # Get base64 encoded data for all resources (CSS, JS, images)
 for tag in soup.find_all(["link", "script", "img"]):
     if tag.name == "img" and tag.get("src"):
         img_url = tag["src"]
-        if not img_url.startswith("http"):
-            img_url = driver.execute_script(
-                f"return new URL('{img_url}', window.location.href).href"
-            )
-        print(img_url)
-        print("============")
-        try:
-            img_data = driver.execute_script(
-                f"return fetch('{img_url}').then(response => response.blob()).then(blob => new Promise((resolve, reject) => {{"
-                f"    const reader = new FileReader();"
-                f"    reader.onloadend = () => resolve(reader.result);"
-                f"    reader.onerror = reject;"
-                f"    reader.readAsDataURL(blob);"
-                f"}}));"
-            )
-        except:
-            img_data = ""
-
-        tag["src"] = img_data
+        tag["src"] = fetch_image(img_url)
     elif tag.name == "script" and tag.get("src"):
         js_url = tag["src"]
-        if not js_url.startswith("http"):
-            js_url = driver.execute_script(
-                f"return new URL('{js_url}', window.location.href).href"
-            )
-        try:
-            js_content = driver.execute_script(
-                f"return fetch('{js_url}').then(response => response.text());"
-            )
-        except:
-            js_content = ""
-        tag.string = js_content
+        tag.string = fetch_text(js_url)
+        del tag["src"]
     elif tag.name == "link" and tag.get("rel") == ["stylesheet"] and tag.get("href"):
         css_url = tag["href"]
-        if css_url.startswith("http"):
-            css_content = driver.execute_script(
-                f"return fetch('{css_url}').then(response => response.text());"
-            )
-        else:
-            absolute_css_url = driver.execute_script(
-                f"return new URL('{css_url}', window.location.href).href"
-            )
-            css_content = driver.execute_script(
-                f"return fetch('{absolute_css_url}').then(response => response.text());"
-            )
         style_tag = soup.new_tag("style")
-        style_tag.string = css_content
+        style_tag.string = fetch_text(css_url)
         tag.replace_with(style_tag)
 
 # Close the browser
